d1ddd9469484a32783396b2c27cf2de83accc671
ravi7299 <43462462+ravi7299@users.noreply.github.com>
2019-10-17 02:04:10 +0530

    LearningCurves function completed

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -52,7 +52,14 @@ error_val   = zeros(m, 1);
 %
 
 % ---------------------- Sample Solution ----------------------
-
+for i=1:m
+    theta = trainLinearReg(X(1:i,:), y(1:i), lambda);
+    [J_train,~] = linearRegCostFunction(X(1:i,:), y(1:i), theta, 0);
+    error_train(i)=J_train;
+    
+    [J_val,~] = linearRegCostFunction(Xval, yval, theta, 0);
+    error_val(i)=J_val;
+end    
 
 
 
